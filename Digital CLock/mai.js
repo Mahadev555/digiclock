ec75369e4e6eed682c5bd605a0c2b21c8dd544ae
Mahadev abhay manerikar <82248918+Mahadev555@users.noreply.github.com>
2023-02-18 11:27:20 +0530

    Add files via upload

---
+++ Digital CLock/mai.js
@@ -0,0 +1,36 @@
+const hour1 = document.getElementById("hour")
+const minutes1 = document.getElementById("minutes")
+const seconds1 = document.getElementById("seconds")
+const ampm1 = document.getElementById("ampm")
+
+function clockUpdate(){
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM"
+
+    if(h>12){
+        h = h - 12
+        ampm = "PM"
+    }
+
+    h = h < 10 ? "0"+h : h;
+    m = m < 10 ? "0"+m : m;
+    s = s < 10 ? "0"+s : s;
+
+    hour1.innerText = h;
+    minutes1.innerText = m;
+    seconds1.innerText = s;
+    ampm1,(innerText = ampm);
+
+    setTimeout(()=>{
+       clockUpdate()
+
+    },1000)
+
+
+}
+
+clockUpdate();
+ 
+
